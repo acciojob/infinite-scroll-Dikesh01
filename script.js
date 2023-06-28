a618ac7fda8d3ffad08a424f917bcb9eec5f95dc
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-28 13:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-//your code here!
+
+
+    window.addEventListener('DOMContentLoaded', function() {
+      var list = document.getElementById('infi-list');
+      var itemsToAdd = 2;
+
+      function addListItems() {
+        for (var i = 1; i <= itemsToAdd; i++) {
+          var listItem = document.createElement('li');
+          listItem.textContent = 'List item ' + (list.childElementCount + i);
+          list.appendChild(listItem);
+        }
+      }
+
+      function checkScroll() {
+        var lastItem = list.lastElementChild;
+        var lastItemOffset = lastItem.offsetTop + lastItem.offsetHeight;
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var windowHeight = window.innerHeight;
+
+        if (scrollTop + windowHeight >= lastItemOffset) {
+          addListItems();
+        }
+      }
+
+      window.addEventListener('scroll', checkScroll);
+    });
 
